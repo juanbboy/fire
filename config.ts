ff509a38cdddfa90c7b0375d27d1a504db0836bd
Desarrollo Nylon <juanca.rr@hotmail.com>
2024-12-05 12:33:13 -0500

    first commit

---
+++ config.ts
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  // Your web app's Firebase configuration here
+  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+  apiKey: "AIzaSyAi27L65Xb3RAdg-fnPGAoYqfB6E2wHJMY",
+  authDomain: "react-auth-app-6a340.firebaseapp.com",
+  projectId: "react-auth-app-6a340",
+  storageBucket: "react-auth-app-6a340.firebasestorage.app",
+  messagingSenderId: "778269297947",
+  appId: "1:778269297947:web:9320a572a17e7aef7257be",
+  measurementId: "G-S269MPFKEW"
+};
+
+export const vapidKey = 'BC2PiRCzMAZT9PPQiXQoS9pTN63-P_ktTuW_kzdzSpa8IeNSiaOPqzL2j54hRmDw0BjG2eyjv1FsXdscweG9P0s';
